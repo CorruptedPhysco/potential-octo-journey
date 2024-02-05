8a04ea182b7ae6e7179356faf11105433eb2ef7c
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-02-05 19:29:51 +0530

    Update mylab.sh

--- mylab.sh
+++ mylab.sh
@@ -1,97 +1,20 @@
 
 
-gcloud services enable \
-  artifactregistry.googleapis.com \
-  cloudfunctions.googleapis.com \
-  cloudbuild.googleapis.com \
-  eventarc.googleapis.com \
-  run.googleapis.com \
-  logging.googleapis.com \
-  pubsub.googleapis.com
+gcloud compute instances create gcelab --zone $ZONE --machine-type e2-standard-2
 
+gcloud compute disks create mydisk --size=200GB \
+--zone $ZONE
 
-export HTTP_FUNCTION=http-responder
-export FUNCTION_NAME=cs-tracker
-export REGION=us-west1
+gcloud compute instances attach-disk gcelab --disk mydisk --zone $ZONE
 
+gcloud compute ssh gcelab --zone $ZONE
 
+ls -l /dev/disk/by-id/
 
-PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$DEVSHELL_PROJECT_ID" --format='value(project_number)')
-SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
-  --member serviceAccount:$SERVICE_ACCOUNT \
-  --role roles/pubsub.publisher
+sudo mkdir /mnt/mydisk
 
-gsutil mb -l $REGION gs://$DEVSHELL_PROJECT_ID
-export BUCKET="gs://$DEVSHELL_PROJECT_ID"
+sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1
 
-mkdir ~/$FUNCTION_NAME && cd $_
-touch index.js && touch package.json
+sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk
 
-cat > index.js <<EOF
-const functions = require('@google-cloud/functions-framework');
-functions.cloudEvent('$FUNCTION_NAME', (cloudevent) => {
-  console.log('A new event in your Cloud Storage bucket has been logged!');
-  console.log(cloudevent);
-});
-EOF
-
-cat > package.json <<EOF
-{
-  "name": "nodejs-functions-gen2-codelab",
-  "version": "0.0.1",
-  "main": "index.js",
-  "dependencies": {
-    "@google-cloud/functions-framework": "^2.0.0"
-  }
-}
-EOF
-
-gcloud functions deploy $FUNCTION_NAME \
-  --gen2 \
-  --runtime nodejs16 \
-  --entry-point $FUNCTION_NAME \
-  --source . \
-  --region $REGION \
-  --trigger-bucket $BUCKET \
-  --trigger-location $REGION \
-  --max-instances 2
-
-
-
- 
-cd ..
-
-mkdir ~/HTTP_FUNCTION && cd $_
-touch index.js && touch package.json
-
-cat > index.js <<EOF
-const functions = require('@google-cloud/functions-framework');
-functions.http('$HTTP_FUNCTION', (req, res) => {
-  res.status(200).send('subscribe to quikclab');
-});
-EOF
-
-
-cat > package.json <<EOF
-{
-  "name": "nodejs-functions-gen2-codelab",
-  "version": "0.0.1",
-  "main": "index.js",
-  "dependencies": {
-    "@google-cloud/functions-framework": "^2.0.0"
-  }
-}
-EOF
-
-
-gcloud functions deploy $HTTP_FUNCTION \
-  --gen2 \
-  --runtime nodejs16 \
-  --entry-point $HTTP_FUNCTION \
-  --source . \
-  --region $REGION \
-  --trigger-http \
-  --timeout 600s \
-  --max-instances 2 \
-  --min-instances 1
+sudo nano /etc/fstab
