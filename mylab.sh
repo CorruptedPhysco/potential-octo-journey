fbc83dce09110faefe614249844c7200fc743541
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-02-05 19:52:04 +0530

    Update mylab.sh

--- mylab.sh
+++ mylab.sh
@@ -1,1 +1,13 @@
+git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
 
+cd java-docs-samples/appengine-java8/helloworld
+
+mvn clean
+
+mvn package
+
+mvn appengine:run
+
+gcloud app create
+
+nano pom.xml
